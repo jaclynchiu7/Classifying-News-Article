bd5b50ae1730db25dd25cdac5a9eb4225c6b13e2
Jaclyn Chiu <72675054+jaclynchiu7@users.noreply.github.com>
2023-04-06 20:40:00 -0700

    helper function

---
+++ extra.py
@@ -0,0 +1,22 @@
+import re
+
+def clean(text):
+    text = re.sub(r'\d+', '', text)
+    text = re.sub(r'^https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
+    texter = re.sub(r"<br />", " ", text)
+    texter = re.sub(r"&quot;", "\"",texter)
+    texter = re.sub('&#39;', "\"", texter)
+    texter = re.sub('\n', " ", texter)
+    texter = re.sub(' u '," you ", texter)
+    texter = re.sub('`',"", texter)
+    texter = re.sub(' +', ' ', texter)
+    texter = re.sub(r"(!)\1+", r"!", texter)
+    texter = re.sub(r"(\?)\1+", r"?", texter)
+    texter = re.sub('&amp;', 'and', texter)
+    texter = re.sub('\r', ' ',texter)
+    clean = re.compile('<.*?>')
+    texter = texter.encode('ascii', 'ignore').decode('ascii')
+    texter = re.sub(clean, '', texter)
+    if texter == "":
+      texter = ""
+    return texter
